c53e0bea729a4507d45870b264efda155783ce93
Diego Coronel <diegocoronel1997@gmail.com>
2022-01-15 12:23:28 -0500

    remove unused function

--- client/src/hooks/useApi.jsx
+++ client/src/hooks/useApi.jsx
@@ -13,7 +13,6 @@ export default (defaultData, initialLoading) => {
       setData(response);
     } catch (error) {
       setError(error.response?.data);
-      callback(error);
     } finally {
       setIsLoading(false);
     }
